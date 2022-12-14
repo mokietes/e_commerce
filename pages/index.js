38924751e63ec0ca45eeba7a862673ef02a40302
Mekonnen T Tesfazien <mekonnen.tesfamichael@gmail.com>
2022-12-14 03:19:52 -0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,16 @@
 import React from "react";
 
 const index = () => {
-  return <div>index</div>;
+  return (
+    <div>
+      <div className="products-heading">
+        <h2>Best Selling Products</h2>
+        <p>Speakers of many variations</p>
+      </div>
+
+      <div>{["Product 1", "Product 2"].map((product) => product)}</div>
+    </div>
+  );
 };
 
 export default index;
