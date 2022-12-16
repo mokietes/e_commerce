fa16b13e7ef186abf4e799da4f8d7a1c02bcfa9b
Mekonnen T Tesfazien <mekonnen.tesfamichael@gmail.com>
2022-12-16 06:20:58 -0800

    HeroBanner

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,17 @@
 import React from "react";
+import { Product, FooterBanner, HeroBanner } from "../component";
 
 const index = () => {
   return (
     <div>
+      <HeroBanner />
       <div className="products-heading">
         <h2>Best Selling Products</h2>
         <p>Speakers of many variations</p>
       </div>
 
       <div>{["Product 1", "Product 2"].map((product) => product)}</div>
+      <FooterBanner />
     </div>
   );
 };
