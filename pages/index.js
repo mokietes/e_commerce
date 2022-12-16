934ae78c4dc3c0eb69172baea9bb6ccce5a5b1b1
Mekonnen T Tesfazien <mekonnen.tesfamichael@gmail.com>
2022-12-16 06:44:01 -0800

    location of files

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,32 @@
 import React from "react";
 import { Product, FooterBanner, HeroBanner } from "../component";
+import { client } from "../lib/client";
 
-const index = () => {
-  return (
-    <div>
-      <HeroBanner />
-      <div className="products-heading">
-        <h2>Best Selling Products</h2>
-        <p>Speakers of many variations</p>
-      </div>
-
-      <div>{["Product 1", "Product 2"].map((product) => product)}</div>
-      <FooterBanner />
+const Home = ({ products, bannerData }) => (
+  <div>
+    <HeroBanner HeroBanner={} />
+    <div className="products-heading">
+      <h2>Best Selling Products</h2>
+      <p>Speakers of many variations</p>
     </div>
-  );
+
+    <div className="products-container">
+      {products?.map((product) => product.name)}
+    </div>
+    <FooterBanner />
+  </div>
+);
+
+export const getServerSideProps = async () => {
+  const query = '*[_type == "product"]';
+  const products = await client.fetch(query);
+
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery);
+
+  return {
+    props: { products, bannerData },
+  };
 };
 
-export default index;
+export default Home;
