4296aca8d9d7fecbe3d80e22dbe462d0cc031d72
Mekonnen T Tesfazien <mekonnen.tesfamichael@gmail.com>
2022-12-21 13:41:21 -0800

    Update [slug].js

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -12,18 +12,29 @@ import { Product } from "../../components";
 
 const ProductDetails = ({ product, products }) => {
   const { image, name, details, price } = product;
+  const [index, setIndex] = useState(0);
+
   return (
     <div>
       <div className="product-detail-container">
         <div>
           <div className="image-container">
-            <img src={urlFor(image && image[0])} />
+            <img
+              src={urlFor(image && image[index])}
+              className="product-detail-image"
+            />
           </div>
-          {/* <div className="small-images-container">
+          <div className="small-images-container">
             {image?.map((item, i) => (
-              <img src={urlFor(item)} classNamme="" />
+              <img
+                src={urlFor(item)}
+                className={
+                  i === index ? "small-image selected-image" : "small-image"
+                }
+                onMouseEnter={() => setIndex(i)}
+              />
             ))}
-          </div> */}
+          </div>
         </div>
 
         <div className="product-detail-desc">
