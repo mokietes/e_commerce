4ef9a46a6527f021e52f14f4a9629b2550171bdc
Mekonnen T Tesfazien <mekonnen.tesfamichael@gmail.com>
2022-12-25 23:15:01 -0800

    update

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -8,7 +8,7 @@ import {
 
 import { client, urlFor } from "../../lib/client";
 import { Product } from "../../components";
-//import { useStateContext } from "../../context/StateContext";
+import { useStateContext } from "../../context/StateContext";
 
 const ProductDetails = ({ product, products }) => {
   const { image, name, details, price } = product;
