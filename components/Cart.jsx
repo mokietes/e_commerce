844cd4801e064795f2a48008a46c2e85bd66e76c
Mekonnen T Tesfazien <mekonnen.tesfamichael@gmail.com>
2023-01-02 23:08:58 -0800

    Update Cart.jsx

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -51,6 +51,22 @@ const Cart = () => {
           <span className="heading">Your Cart</span>
           <span className="cart-num-items">({totalQuantities} tems)</span>
         </button>
+
+        {cartItems.length < 1 && (
+          <div className="empty-cart">
+            <AiOutlineShopping size={150} />
+            <h3>Your shopping bag is empty</h3>
+            <Link href="/">
+              <button
+                type="button"
+                onClick={() => setShowCart(false)}
+                className="btn"
+              >
+                Continue Shopping
+              </button>
+            </Link>
+          </div>
+        )}
       </div>
     </div>
   );
