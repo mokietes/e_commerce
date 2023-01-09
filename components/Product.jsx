ea4e42e26cf25c1e492a1279459385495fb0108d
Mekonnen T Tesfazien <mekonnen.tesfamichael@gmail.com>
2023-01-08 17:08:03 -0800

    Update Product.jsx

--- components/Product.jsx
+++ components/Product.jsx
@@ -1,3 +1,6 @@
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable @next/next/no-img-element */
+
 import React from "react";
 import Link from "next/link";
 import { urlFor } from "../lib/client";
