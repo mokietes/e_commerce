1b0116c744dc5ca25385a4e83315ae6e25458c20
Mekonnen T Tesfazien <mekonnen.tesfamichael@gmail.com>
2023-01-10 23:31:02 -0800

    Update StateContext.js

--- context/StateContext.js
+++ context/StateContext.js
@@ -44,12 +44,25 @@ export const StateContext = ({ children }) => {
 
   const toggleCartItemQuanitity = (id, value) => {
     foundProduct = cartItems.find((item) => item._id === id);
-    idex = cartItems.findIndex((product) => product._id === id);
+    index = cartItems.findIndex((product) => product._id === id);
+    const newCartItems = cartItems.filter((item) => item._id !== id);
 
     if (value === "inc") {
-      foundProduct.quantity += 1;
-      cartItems[idex] = foundProduct;
-    } else if (value) {
+      setCartItems([
+        ...newCartItems,
+        { ...foundProduct, quantity: foundProduct.quantity + 1 },
+      ]);
+      setTotalPrice((prevTotalPrice) => prevTotalPrice + foundProduct.price);
+      setTotalQuantities((prevTotalQuantities) => prevTotalQuantities + 1);
+    } else if (value === "dec") {
+      if (foundProduct.quantity > 1) {
+        setCartItems([
+          ...newCartItems,
+          { ...foundProduct, quantity: foundProduct.quantity - 1 },
+        ]);
+        setTotalPrice((prevTotalPrice) => prevTotalPrice - foundProduct.price);
+        setTotalQuantities((prevTotalQuantities) => prevTotalQuantities - 1);
+      }
     }
   };
 
