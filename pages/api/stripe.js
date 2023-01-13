faf392348573181204b0c773e4f79851173c749b
Mekonnen T Tesfazien <mekonnen.tesfamichael@gmail.com>
2023-01-12 21:58:05 -0800

    Update stripe.js

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -20,7 +20,21 @@ export default async function handler(req, res) {
             )
             .replace("-webp", ".webp");
 
-          return {};
+          return {
+            price_data: {
+              currency: "usd",
+              product_data: {
+                name: item.name,
+                images: [newImage],
+              },
+              unit_amount: item.price * 100,
+            },
+            adjustable_quantity: {
+              enabled: true,
+              minimum: 1,
+            },
+            quantity: item.quantity,
+          };
         }),
         success_url: `${req.headers.origin}/success`,
         cancel_url: `${req.headers.origin}/canceled`,
