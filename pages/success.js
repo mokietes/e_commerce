3bf4848ddf2034789b9f7328b3bed9f52e9c7ade
Mekonnen T Tesfazien <mekonnen.tesfamichael@gmail.com>
2023-01-16 23:24:21 -0800

    Update success.js

--- pages/success.js
+++ pages/success.js
@@ -4,3 +4,19 @@ import { BsBagCheckFill } from "react-icons/bs";
 
 import { useStateContext } from "../context/StateContext";
 import { runFireworks } from "../lib/utils";
+
+const Success = () => {
+  const { setCartItems, setTotalPrice, setTotalQuantities } = useStateContext();
+
+  useEffect(() => {
+    localStorage.clear();
+  }, []);
+
+  return (
+    <div className="success-wrapper">
+      <div className="success"></div>
+    </div>
+  );
+};
+
+export default Success;
