08a7965d5982b51150377ac2356d149d0878f5f3
Mekonnen T Tesfazien <mekonnen.tesfamichael@gmail.com>
2023-01-17 10:29:48 -0800

    Update success.js

--- pages/success.js
+++ pages/success.js
@@ -18,7 +18,19 @@ const Success = () => {
 
   return (
     <div className="success-wrapper">
-      <div className="success"></div>
+      <div className="success">
+        <p className="icon">
+          <BsBagCheckFill />
+        </p>
+        <h2>Thank you for your order!</h2>
+        <p className="email-msg">Check your email inbox for the receipt.</p>
+        <p className="description">
+          If you have any questions, please email
+          <a className="email" href="mailto:order@example.com">
+            order@example.com
+          </a>
+        </p>
+      </div>
     </div>
   );
 };
